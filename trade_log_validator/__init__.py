297dddd077a16206dc78b289061ec796d44b0611
Aryan <aryangupta07075@gmail.com>
2025-12-10 14:58:26 +0530

    input handling

--- trade_log_validator/__init__.py
+++ trade_log_validator/__init__.py
@@ -1,7 +1,16 @@
 from .functional_main import main
 
 class _ValidatorModule:
-    def __call__(self, algo_name, trade_log_path, lot_size, segment="UNIVERSAL", options_file_path=None):
+    def __call__(self, algo_name="algo_name", trade_log_path=None, lot_size=None, segment="UNIVERSAL", options_file_path=None):
+        if trade_log_path == None:
+            return "Trade Log path not provided"
+        if options_file_path == None:
+            print("[WARNING] options file path not provided will lead the program to skip checks")
+        if (segment.upper() == "OPTIONS") or (segment.upper() == "OPTION") :
+            segment = "OPTIONS"
+            if lot_size == None:
+                print("[WARNING] Contract Lot size not defined using default value 75.")
+            
         return main(algo_name, trade_log_path, options_file_path, lot_size, segment)
 
 # Replace this module with a callable instance
