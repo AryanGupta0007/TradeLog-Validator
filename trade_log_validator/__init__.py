225182b030bc120302443dbd0786b7f4aa3169d6
Aryan <aryangupta07075@gmail.com>
2025-12-10 13:39:59 +0530

    update

--- trade_log_validator/__init__.py
+++ trade_log_validator/__init__.py
@@ -1,10 +1,9 @@
 from .functional_main import main
 
-def __call__(
-    algo_name,
-    trade_log,
-    options_file,
-    lot_size,
-    segment="UNIVERSAL"
-):
-    return main(algo_name, trade_log, options_file, lot_size, segment)
+class _ValidatorModule:
+    def __call__(self, algo_name, trade_log, options_file, lot_size, segment="UNIVERSAL"):
+        return main(algo_name, trade_log, options_file, lot_size, segment)
+
+# Replace this module with a callable instance
+import sys
+sys.modules[__name__] = _ValidatorModule()
