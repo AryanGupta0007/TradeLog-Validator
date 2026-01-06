882200896fa71dec492834bc76a6d2b1795a6cd7
Aryan <aryangupta07075@gmail.com>
2026-01-06 16:44:40 +0530

    update

--- trade_log_validator/universal_checks.py
+++ trade_log_validator/universal_checks.py
@@ -289,9 +289,6 @@ def entry_exit_price_chain_check(df: pl.DataFrame, ORB_URL, ACCESS_TOKEN) -> Che
     issues[result_name] = [('idx', 'Key', 'ExitTime', 'Symbol', 'EntryPrice', 'ExitPrice', 'Quantity', 'PositionStatus', 'Pnl', 'ExitType', 'KeyEpoch', 'ExitEpoch')]
     queries = generate_queries(df=pdf)
     res_df = _get_price(queries=queries, ORB_URL=ORB_URL, ACCESS_TOKEN=ACCESS_TOKEN)
-    print(res_df)
-    import sys 
-    sys.exit()
     for idx, row in pdf.iterrows():
         # Handle NaN values in KeyEpoch and ExitEpoch
         try:
